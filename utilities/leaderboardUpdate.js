086e7fe83df6c0d5db48e6a5d6435258c9ceeaf8
Larry Barrett <larry.c.barrett@gmail.com>
2016-08-06 17:38:44 +0000

    starting leaderboardUpdate chron script

---
+++ utilities/leaderboardUpdate.js
@@ -0,0 +1,96 @@
+db = new Mongo().getDB('horse');
+
+var dateObj = new Date();
+var year = dateObj.getFullYear();
+var month = dateObj.getMonth() + 1;
+var date = dateObj.getDate();
+
+if(month < 10) {
+	month = '0' + month;
+}
+
+if(date < 10) {
+	date = '0' + date;
+}
+
+var useDate = year + month + date;
+
+getTracks(useDate);
+
+function getTracks(useDate) {
+	var cursor = db.trds.find({raceDate: parseInt(useDate)});
+	while(cursor.hasNext()) {
+		var trdData = cursor.next();
+		var trackId = (trdData._id.toString()).substring(10,34);
+		getTournamentsByAssocTrackId(trackId);
+	}
+}
+
+function getTournamentsByAssocTrackId(trackId) {
+	var cursor = db.tournaments.find({assocTrackId: trackId});
+	while(cursor.hasNext()) {
+		var tournamentData = cursor.next();
+		var tournamentId = (tournamentData._id.toString()).substring(10,34);
+		var tournamentName = tournamentData.name;
+		getTournamentStandingsByTournamentId(tournamentId);
+	}
+}
+
+function getTournamentStandingsByTournamentId(tournamentId) {
+	var newCustomers = [];
+	var cursor = db.tournamentstandings.find({tournamentId: tournamentId});
+	while(cursor.hasNext()) {
+		var tsData = cursor.next();
+		tsData.customers.forEach(function(customer) {
+			var thisCustomer = {};
+			thisCustomer.customerId = customer.customerId;
+			thisCustomer.credits = getCustomerTournamentCredits(customer.customerId, tournamentId);
+			newCustomers.push(thisCustomer);
+		});
+print(' ');
+print('tournamentId: '+tournamentId);
+print('newCustomers:');
+print(JSON.stringify(newCustomers));
+		db.tournamentstandings.update(
+			{tournamentId: tournamentId},
+			{customers: newCustomers},
+			false,
+			false
+		)
+	}
+}
+
+function getCustomerTournamentCredits(customerId, tournamentId) {
+	var credits = 500;
+	var cursor = db.wagers.find({
+		customerId: customerId,
+		tournamentId: tournamentId
+	});
+	while(cursor.hasNext()) {
+		var wager = cursor.next();
+		if(wager.scored) {
+			credits += parseFloat(wager.result - wager.wagerTotal);
+		} else {
+			if(!wager.cancelled) {
+				credits -= parseFloat(wager.wagerTotal);
+			}
+		}
+	}
+print(' ');
+print('credits: '+credits);
+	return credits;
+}
+
+function randomIntFromInterval(min,max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+
+
+
+
+
+
+
+
